f34ff38c66b32c70efc9b3891d7698d449d224bc
Ricardo <ricardo_slb_brites@live.ca>
2023-02-22 00:15:34 -0500

    Added cell creation

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from window import Window, Line, Point
+from window import Window
+from cell import Cell
 
 
 def main():
     window = Window(800, 600)
-    line = Line(Point(20, 40), Point(200, 400))
-    window.draw_line(line, "black")
+    cell1 = Cell(window)
+    cell2 = Cell(window)
+    cell1.has_bottom_wall = False
+    cell1.draw(100, 200, 300, 400)
+    cell2.has_right_wall = False
+    cell2.draw(500, 600, 400, 500)
     window.wait_for_close()
 
 
