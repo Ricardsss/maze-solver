f58c3411abb04ed73d728cc3e685402802df915a
Ricardo <ricardo_slb_brites@live.ca>
2023-02-26 23:04:03 -0500

    Added unit tests

--- maze.py
+++ maze.py
@@ -11,7 +11,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win=None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -30,6 +30,8 @@ class Maze:
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
+        if self._win is None:
+            return
         x1 = i * self._cell_size_x + self._x1
         x2 = x1 + self._cell_size_x
         y1 = j * self._cell_size_y + self._y1
@@ -38,5 +40,7 @@ class Maze:
         self._animate()
 
     def _animate(self):
+        if self._win is None:
+            return
         self._win.redraw()
         time.sleep(0.05)
